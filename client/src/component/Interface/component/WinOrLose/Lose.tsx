4f6a3b9261ef47641d65c8ee238792e04618c01d
AnnaTonkova <anatonkova02@gmail.com>
2024-05-26 17:51:49 +0400

    fix

--- client/src/component/Interface/component/WinOrLose/Lose.tsx
+++ client/src/component/Interface/component/WinOrLose/Lose.tsx
@@ -1,4 +1,17 @@
+import React from "react";
+import "./Lose.css";
 const Lose: React.FC = () => {
-    return <></>;
+    return <>
+        <div className="image-lose"></div>
+            <div className="text-lose">
+            ПОРАЖЕНИЕ
+        </div>
+        <div className="text-lose-1">
+            Поздравляю, ты отчислен
+        </div>
+        <div className="text-lose-2">
+            Добро пожаловать в армию
+        </div>
+    </>;
 };
 export default Lose;
--- client/src/component/Interface/component/WinOrLose/Win.tsx
+++ client/src/component/Interface/component/WinOrLose/Win.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import "./Win.css"
 const Win: React.FC = () => {
     return <>
-        <div>
         <div className="image-win"></div>
+            <div className="text-win">
+            ПОБЕДА
+        </div>
+        <div className="text-win-1">
+            Поздравляю, ты закрыл долги
+        </div>
+        <div className="text-win-2">
+            Добро пожаловать в успешную жизнь
         </div>
     </>;
 };
