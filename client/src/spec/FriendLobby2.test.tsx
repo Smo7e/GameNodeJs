72082cc7de3c9f80479fcce171443edaec6c1e99
Razan Ali <razanali627@gmail.com>
2024-05-29 20:49:33 +0400

    fix

--- client/src/spec/FriendLobby2.test.tsx
+++ client/src/spec/FriendLobby2.test.tsx
@@ -8,13 +8,62 @@ const store = new Store();
 const mediator = new Mediator(MEDIATOR);
 const server = new Server(HOST, mediator, store);
 
-test("FriendLobby2 snapshot", () => {
+// test("FriendLobby2 snapshot gamerNumber 0", () => {
+    
+//     const { asFragment } = render(
+//         <MediatorContext.Provider value={mediator}>
+//             <ServerContext.Provider value={server}>
+//                 <FriendLobby2 setPanel={() => {}} gamers={[]}  />
+//             </ServerContext.Provider>
+//         </MediatorContext.Provider>
+//     );
+//     expect(asFragment()).toMatchSnapshot();
+// });
+
+describe('FriendLobby2 component', () => {
+    test('renders correctly with Admin post and gamers length <= 2', () => {
+        const { asFragment } = render(
+                    <MediatorContext.Provider value={mediator}>
+                        <ServerContext.Provider value={server}>
+                            <FriendLobby2 setPanel={() => {}} gamers={[]}  />
+                        </ServerContext.Provider>
+                    </MediatorContext.Provider>
+                );
+                expect(asFragment()).toMatchSnapshot();
+    });
+    test("FriendLobby2 snapshot gamerNumber 0", () => {
+        const gamers = [{ person_id: 0 }, { person_id: 1 }, { person_id: 2 }];
     const { asFragment } = render(
         <MediatorContext.Provider value={mediator}>
             <ServerContext.Provider value={server}>
-                {/* <FriendLobby2 setPanel={() => {}} gamers /> */}
+                <FriendLobby2 setPanel={() => {}} gamers={[]}  />
             </ServerContext.Provider>
         </MediatorContext.Provider>
     );
     expect(asFragment()).toMatchSnapshot();
+});
+test("FriendLobby2 snapshot gamerNumber 1", () => {
+    const gamers = [{ person_id: 0 }, { person_id: 1 }];
+    const { asFragment } = render(
+        <MediatorContext.Provider value={mediator}>
+            <ServerContext.Provider value={server}>
+                <FriendLobby2 setPanel={() => {}} gamers={[]}  />
+            </ServerContext.Provider>
+        </MediatorContext.Provider>
+    );
+    expect(asFragment()).toMatchSnapshot();
+});
+
+test("FriendLobby2 snapshot gamerNumber 2", () => {
+    const gamers = [{ person_id: 0 }, { person_id: 1 }, { person_id: 3 }];
+    const { asFragment } = render(
+        <MediatorContext.Provider value={mediator}>
+            <ServerContext.Provider value={server}>
+                <FriendLobby2 setPanel={() => {}} gamers={[]}  />
+            </ServerContext.Provider>
+        </MediatorContext.Provider>
+    );
+    expect(asFragment()).toMatchSnapshot();
+});
+
 });
